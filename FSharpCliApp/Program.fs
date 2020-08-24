f0e425acb58e38180b79b43aae7f847cc12394f1
Abhinav Sharma <abhi18av@gmail.com>
2020-08-24 22:15:08 +0530

    implemented the base functionality of sayHello

--- FSharpCliApp/Program.fs
+++ FSharpCliApp/Program.fs
@@ -4,15 +4,13 @@ open Argu
 type CliError =
     | ArgumentsNotSpecified
 
-type CmdArgs =
-    | [<AltCommandLine("-p")>] Print of message:string
-    | [<AltCommandLine("-e")>] Echo of message:string
+type Name =
+    | [<AltCommandLine("-n")>] Name of name:string
 with
     interface IArgParserTemplate with
         member this.Usage =
             match this with
-            | Print _ -> "Print a message"
-            | Echo _ -> "Echo a word"
+            | Name _ -> "Name of the guest"
 
 let getExitCode result =
     match result with
@@ -21,24 +19,23 @@ let getExitCode result =
         match err with
         | ArgumentsNotSpecified -> 1
 
-let runPrint print = 
-    printfn "%s" print
+let sayName name = 
+    printfn "Hello, %s!" name
     Ok ()
 
 
-let runEcho echo = 
-    printfn echo
-    Ok ()
 
 
 [<EntryPoint>]
 let main argv = 
     let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
-    let parser = ArgumentParser.Create<CmdArgs>(programName = "FSharpCliApp", errorHandler = errorHandler)
-    
+    let parser = ArgumentParser.Create<Name>(programName = "FSharpCliApp", errorHandler = errorHandler)
+
+//    let results = parser.ParseCommandLine argv
+//    printfn "Got parse results %A" <| results.GetAllResults()
+
     match parser.ParseCommandLine argv with
-    | p when p.Contains(Print) -> runPrint (p.GetResult(Print))
-    | e when e.Contains(Echo) -> runPrint (e.GetResult(Echo))
+    | p when p.Contains(Name) -> sayName (p.GetResult(Name))
     | _ ->
         printfn "%s" (parser.PrintUsage())
         Error ArgumentsNotSpecified
